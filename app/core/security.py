9528c90e5de02c78a80daec421d49bbe64f6dfc7
Alexander Karnaukhov <cookie.the.cook.5.65@gmail.com>
2025-06-02 21:17:02 +0400

    fix tz

--- app/core/security.py
+++ app/core/security.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 
 from jose import JWTError, jwt
 from passlib.context import CryptContext
@@ -15,9 +15,9 @@ def create_jwt_token(
 ):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(
         to_encode, secret_key or settings.secret_key, algorithm=settings.algorithm
