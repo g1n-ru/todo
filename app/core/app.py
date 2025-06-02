97d3465dd5a5c22d0b2de4f7101599a95bab0612
Alexander Karnaukhov <cookie.the.cook.5.65@gmail.com>
2025-06-02 21:07:36 +0400

    cors

--- app/core/app.py
+++ app/core/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.auth.api import router as auth_router
 from app.categories.api import router as categories_router
@@ -8,6 +9,8 @@ from app.users.api import router as users_router
 
 def create_app():
     app = FastAPI()
+
+    app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_credentials=True, allow_headers=["*"])
 
     app.include_router(auth_router, prefix="/api/v1", tags=["auth"])
     app.include_router(tasks_router, prefix="/api/v1", tags=["tasks"])
